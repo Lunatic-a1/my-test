be61c3ccc3d1d2a93c4edecbdf24e8ab8318d80e
Oyuk <iluuacc11@gmail.com>
2025-06-27 15:45:24 +0800

    Create load-layout.js

---
+++ scripts/load-layout.js
@@ -0,0 +1,18 @@
+function loadLayout() {
+    // 헤더 불러오기
+    fetch("/includes/header.html")
+      .then(res => res.text())
+      .then(data => {
+        document.body.insertAdjacentHTML("afterbegin", data);
+      });
+  
+    // 푸터 불러오기
+    fetch("/includes/footer.html")
+      .then(res => res.text())
+      .then(data => {
+        document.body.insertAdjacentHTML("beforeend", data);
+      });
+  }
+  
+  window.addEventListener("DOMContentLoaded", loadLayout);
+  
