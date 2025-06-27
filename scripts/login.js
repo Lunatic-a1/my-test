a4b9fb59bb721cb5a334e4002ffb3ee720f085d7
Oyuk <iluuacc11@gmail.com>
2025-06-27 22:12:53 +0800

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,19 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-analytics.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAdc-uOw-RkwioP-GFf1BouVu4JBkfGFnU",
-  authDomain: "login-4cb47.firebaseapp.com",
-  projectId: "login-4cb47",
-  storageBucket: "login-4cb47.firebasestorage.app",
-  messagingSenderId: "866437023451",
-  appId: "1:866437023451:web:c41f1a1b07accf50ae2efe",
-  measurementId: "G-EW136C5LGV"
-};
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
+import { auth } from "./firebase-init.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
 
 const form = document.getElementById('login-form');
 const messageDiv = document.getElementById('login-message');
