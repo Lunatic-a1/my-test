3b26eaf74455af14bf9a793a9dd10c033262d96b
Oyuk <iluuacc11@gmail.com>
2025-06-27 18:13:42 +0800

    Create login.js

---
+++ login.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdc-uOw-RkwioP-GFf1BouVu4JBkfGFnU",
+  authDomain: "login-4cb47.firebaseapp.com",
+  projectId: "login-4cb47",
+  storageBucket: "login-4cb47.firebasestorage.app",
+  messagingSenderId: "866437023451",
+  appId: "1:866437023451:web:c41f1a1b07accf50ae2efe",
+  measurementId: "G-EW136C5LGV"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+const form = document.getElementById('login-form');
+const messageDiv = document.getElementById('login-message');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = form.email.value;
+  const password = form.password.value;
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      messageDiv.textContent = '로그인 성공!';
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      messageDiv.textContent = '로그인 실패: ' + errorMessage;
+    });
+}); 
