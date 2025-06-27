3311fe61aff3c8201fc37a0deae2990bb8cb50c4
Oyuk <iluuacc11@gmail.com>
2025-06-27 22:12:49 +0800

    Update header.js

--- scripts/header.js
+++ scripts/header.js
@@ -1,17 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAdc-uOw-RkwioP-GFf1BouVu4JBkfGFnU",
-  authDomain: "login-4cb47.firebaseapp.com",
-  projectId: "login-4cb47",
-  storageBucket: "login-4cb47.firebasestorage.app",
-  messagingSenderId: "866437023451",
-  appId: "1:866437023451:web:c41f1a1b07accf50ae2efe",
-  measurementId: "G-EW136C5LGV"
-};
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+import { auth } from "./firebase-init.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
 
 function bindHeaderAuthEvents() {
   const logoutBtn = document.getElementById('logout-btn');
