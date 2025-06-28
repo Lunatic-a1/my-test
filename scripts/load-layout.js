3b9ecdfed840df83d942db4710d33156ad5d3ab0
Oyuk <iluuacc11@gmail.com>
2025-06-29 04:30:16 +0800

    Update load-layout.js

--- scripts/load-layout.js
+++ scripts/load-layout.js
@@ -1,13 +1,13 @@
 function loadLayout() {
     // 헤더 불러오기
-    fetch("/includes/header.html")
+    fetch("includes/header.html")
       .then(res => res.text())
       .then(data => {
         document.body.insertAdjacentHTML("afterbegin", data);
       });
   
     // 푸터 불러오기
-    fetch("/includes/footer.html")
+    fetch("includes/footer.html")
       .then(res => res.text())
       .then(data => {
         document.body.insertAdjacentHTML("beforeend", data);
