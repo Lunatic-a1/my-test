b5ff5b351f54de6fa927662ed3dc4bd97d8b0a47
Oyuk <iluuacc11@gmail.com>
2025-06-29 18:24:22 +0800

    Update favorites.js

--- scripts/favorites.js
+++ scripts/favorites.js
@@ -18,20 +18,20 @@ function renderFavorites(page = 1) {
   const pageFavorites = favorites.slice(startIdx, endIdx);
   pageFavorites.forEach(fav => {
     const card = document.createElement('div');
-    card.className = 'post-card favorites-post-card';
+    card.className = 'content-card favorites-content-card';
     card.innerHTML = `
-      <div class="poster-box">포스터</div>
-      <div class="post-info">
-        <div class="post-title favorites-post-title">${fav.up ? '<span class=\'up-badge\'>UP</span>' : ''}${fav.title}</div>
-        <div class="post-meta">
-          <span class="meta-icon" aria-label="rating"> 
+      <div class="content-poster-box">포스터</div>
+      <div class="content-info">
+        <div class="content-title favorites-content-title">${fav.up ? '<span class=\'up-badge\'>UP</span>' : ''}${fav.title}</div>
+        <div class="content-meta">
+          <span class="content-icon" aria-label="rating"> 
             <svg width="1em" height="1em" viewBox="0 0 20 20" fill="none"><path d="M10 15.27L16.18 19l-1.64-7.03L20 7.24l-7.19-.61L10 0 7.19 6.63 0 7.24l5.46 4.73L3.82 19z" fill="#bbb"/></svg>
           </span>
-          <span class="meta-value">${fav.rating}</span>
-          <span class="meta-icon" aria-label="views">
+          <span class="content-value">${fav.rating}</span>
+          <span class="content-icon" aria-label="views">
             <svg width="1em" height="1em" viewBox="0 0 20 20" fill="none"><path d="M10 4C4 4 1 10 1 10s3 6 9 6 9-6 9-6-3-6-9-6zm0 10c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm0-6a2 2 0 100 4 2 2 0 000-4z" fill="#bbb"/></svg>
           </span>
-          <span class="meta-value">${fav.views}</span>
+          <span class="content-value">${fav.views}</span>
         </div>
       </div>
     `;
